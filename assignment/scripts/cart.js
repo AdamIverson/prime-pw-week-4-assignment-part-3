8567dfb7a43fa678f64d55d685fc3afa46c09ca7
Adam Iverson <aciverso@gmail.com>
2021-10-14 19:56:54 -0500

    through listItems plus one undefined

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem( item ){
+  basket.push( item );
+  return true;
+}
+
+console.log(addItem("socks"));
+console.log(addItem("truffle candles"));
+console.log(addItem("one large rock"));
+console.log(basket);
+
+function listItems( basket ){
+let myArray = basket.length;
+
+  for (let i=0; i<myArray; i++){
+    console.log(basket[i]);
+  }
+}
+
+console.log(listItems( basket ));
+console.log(`Basket is ${basket}`);
